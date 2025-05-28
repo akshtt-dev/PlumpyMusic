fd9d741dc2d966834936108b3f79ffa729de3767
akshtt-dev <akshtt.dev@gmail.com>
2025-05-28 18:04:34 +0530

    Fix play command to handle SoundCloud tracks and improve error messages

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,15 +1,11 @@
-// TODO: Currently the play command only supports youtube, it gives an error if you try to play a soundcloud track. This is because it tries the split method on the uri which is not a youtube url. Fix this by checking if the uri is a youtube url before trying to split it. Also add support for soundcloud tracks.
-
 import { MessageFlags, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
 export default {
   data: new SlashCommandBuilder()
     .setName("play")
     .setDescription("Play a song")
     .addStringOption((option) =>
-      option
-        .setName("query")
-        .setDescription("The song to play")
-        .setRequired(true)
+      option.setName("query").setDescription("The song to play").setRequired(true)
     )
     .addStringOption((option) =>
       option
@@ -26,14 +22,14 @@ export default {
     await interaction.deferReply();
     const guild = interaction.guild;
     const member = interaction.member;
-    const channel = member.voice.channel;
+    const channel = member.voice?.channel;
     const track = interaction.options.getString("query");
     const source = interaction.options.getString("source") || "youtube";
 
     const embed = new EmbedBuilder();
 
     if (!channel) {
-      embed.setDescription("❌ | You are not connected to an audio channel.");
+      embed.setDescription("❌ | You are not connected to a voice channel.");
       embed.setColor("Red");
       return interaction.editReply({
         embeds: [embed],
@@ -41,11 +37,17 @@ export default {
       });
     }
 
-    if (
-      guild.members.me.voice.channel &&
-      member.voice.channelId !== guild.members.me.voice.channelId
-    ) {
-      embed.setDescription("❌ | You are not on the same audio channel as me.");
+    const botChannel = guild.members.me.voice?.channel;
+
+    // Fix: Handle deleted bot channel case
+    if (botChannel && !guild.channels.cache.has(botChannel.id)) {
+      // Disconnect the ghost player
+      const ghostPlayer = interaction.client.lavashark.players.get(guild.id);
+      if (ghostPlayer) await ghostPlayer.destroy();
+    }
+
+    if (botChannel && botChannel.id !== channel.id) {
+      embed.setDescription("❌ | You are not in the same voice channel as me.");
       embed.setColor("Red");
       return interaction.editReply({
         embeds: [embed],
@@ -78,11 +80,9 @@ export default {
       await player.connect();
     } catch (error) {
       console.log(error);
-      embed.setDescription("❌ | I can't join voice channel.");
+      embed.setDescription("❌ | I can't join the voice channel.");
       embed.setColor("Red");
-      return interaction.editReply({
-        embeds: [embed],
-      });
+      return interaction.editReply({ embeds: [embed] });
     }
 
     if (res.loadType === "playlist") {
@@ -93,19 +93,28 @@ export default {
       embed.setColor("Blurple");
       interaction.editReply({ embeds: [embed] });
     } else {
-      embed.setDescription(`Added \`${res.tracks[0].title}\``);
+      const trackInfo = res.tracks[0];
+      let thumbnail = "";
+
+      if (source === "youtube" && trackInfo.uri.includes("v=")) {
+        try {
+          const videoId = new URL(trackInfo.uri).searchParams.get("v");
+          thumbnail = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+        } catch {
+          thumbnail = "";
+        }
+      }
+
+      embed.setDescription(`🎵 Added \`${trackInfo.title}\``);
       embed.setColor("Blurple");
-      embed.setThumbnail(
-        `https://img.youtube.com/vi/${
-          res.tracks[0].uri.split("v=")[1].split("&")[0]
-        }/maxresdefault.jpg`
-      );
+      if (thumbnail) embed.setThumbnail(thumbnail);
       embed.setFooter({
         text: `Requested by ${interaction.user.tag}`,
         iconURL: interaction.user.displayAvatarURL(),
       });
+
       interaction.editReply({ embeds: [embed] });
-      await player.addTracks(res.tracks[0], interaction.user);
+      await player.addTracks(trackInfo, interaction.user);
     }
 
     if (!player.playing) await player.play();
